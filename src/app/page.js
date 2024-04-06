2b407a10e28a5a2302832c6e49f82cef9ed89901
Полубарьев М.Н <mbarev@fgit.ru>
2024-04-06 19:29:57 +0300

    ++

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Home_V1 from "./(home)/home-v1/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
 export const metadata = {
-  title: "Home v1 || Homez - Real Estate NextJS Template",
+  title: "LUMMYO || Real Estate ",
 };
 
 export default function MainRoot() {
