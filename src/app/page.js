dd42955689bc99d11007b0ef3bd7a8a49a7696e9
habdevs <habdevs@yandex.ru>
2024-05-03 19:49:46 +0500

    feat: added figma section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Home_V1 from "./(home)/home-v1/page";
+import Home_V2 from "./(home)/home-v2/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
 export const metadata = {
@@ -8,7 +8,7 @@ export const metadata = {
 export default function MainRoot() {
   return (
     <Wrapper>
-      <Home_V1 />
+      <Home_V2 />
     </Wrapper>
   );
 }
